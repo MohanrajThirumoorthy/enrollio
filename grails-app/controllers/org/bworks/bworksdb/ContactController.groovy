1ffcd433029fce2fc94ac367cf9e68d677cbc81a
Nathan Neff <nathan.neff@gmail.com>
2011-01-20 19:39:28 -0600

    Nuke contact/list page

--- grails-app/controllers/org/bworks/bworksdb/ContactController.groovy
+++ grails-app/controllers/org/bworks/bworksdb/ContactController.groovy
@@ -104,7 +104,7 @@ class ContactController {
         }
         else {
             flash.message = "Contact not found with id ${params.id}"
-            redirect(action:list)
+            redirect(action:list, controller:'student')
         }
     }
 
